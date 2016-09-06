fd233118affec00764c6db24a6feba611b915420
Sanborn Hilland <sanbornh@newyyz.com>
2016-09-06 12:09:50 -0400

    Refactor logger

--- src/logger.js
+++ src/logger.js
@@ -1,16 +1,10 @@
 import isoFormat from './isoFormat.js';
 
-/**
- * @constructs logger
- * @returns logger
- */
 function createLogger (className) {
-
-  var properties = ['log', 'info', 'warn', 'error', 'debug'];
 
   var logger = {};
 
-  properties.forEach(function (property) {
+  ['log', 'info', 'warn', 'error', 'debug'].forEach(function (property) {
     Object.defineProperty(logger, property, {
       get () {
         return console[property].bind(console, isoFormat(new Date), `[${className}]`);
