6f7c32633261ba264e2e11781dccf13eada856aa
Sanborn Hilland <sanbornh@newyyz.com>
2016-09-05 21:56:32 -0400

    Fix embarrassing bug

--- src/logger.js
+++ src/logger.js
@@ -8,20 +8,12 @@ function createLogger (className) {
 
   var properties = ['log', 'info', 'warn', 'error', 'debug'];
 
-  function time () {
-    return isoFormat(new Date);
-  }
-
-  function className () {
-    return `[${className}]`
-  }
-
   var logger = {};
 
   properties.forEach(function (property) {
     Object.defineProperty(logger, property, {
       get () {
-        return console[property].bind(console, time(), className());
+        return console[property].bind(console, isoFormat(new Date), `[${className}]`);
       }
     });
   });
